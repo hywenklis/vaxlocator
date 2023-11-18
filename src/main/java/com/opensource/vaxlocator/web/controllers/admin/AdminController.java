35d98e15d46a4d96d7582e433c1706fce02fdd03
Hywenklis <hywenklis@hotmail.com>
2023-11-18 12:46:47 -0300

    Adiciona descrições no endpoint

--- src/main/java/com/opensource/vaxlocator/web/controllers/admin/AdminController.java
+++ src/main/java/com/opensource/vaxlocator/web/controllers/admin/AdminController.java
@@ -4,6 +4,8 @@ import com.opensource.vaxlocator.integrations.opencagedata.dtos.demas.Establishm
 import com.opensource.vaxlocator.integrations.opencagedata.dtos.opencagedata.OpenCageDataDto;
 import com.opensource.vaxlocator.integrations.opencagedata.service.DemasService;
 import com.opensource.vaxlocator.integrations.opencagedata.service.OpenCageDataService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,8 +25,15 @@ public class AdminController {
   private final DemasService demasService;
 
   @GetMapping("/open/cage/data")
+  @Operation(
+      summary = "Find address information",
+      description = "Retrieves address information using the OpenCageData API: <a href='https://opencagedata.com/' target='_blank'>https://opencagedata.com/</a>"
+  )
   public ResponseEntity<OpenCageDataDto> findAddressInformation(
+      @Parameter(description = "API key for authentication", required = true)
       @RequestParam() String apiKey,
+
+      @Parameter(description = "Postal code for address lookup", required = true)
       @RequestParam("postalCode") String query
   ) {
     return ResponseEntity.status(HttpStatus.OK)
@@ -32,10 +41,21 @@ public class AdminController {
   }
 
   @GetMapping("/demas/gov")
+  @Operation(
+      summary = "Find establishments information",
+      description = "Retrieves establishments information using the Demas API: <a href='https://apidadosabertos.saude.gov.br/v1/' target='_blank'>https://apidadosabertos.saude.gov.br/v1/</a>"
+  )
   public ResponseEntity<EstablishmentsInfoDto> findEstablishmentsInformation(
+      @Parameter(description = "Code representing the type of unity", required = true)
       @RequestParam Integer unityTypeCode,
+
+      @Parameter(description = "Code representing the state", required = true)
       @RequestParam Integer ufCode,
+
+      @Parameter(description = "Maximum number of results to retrieve = 20", required = true)
       @RequestParam Integer limit,
+
+      @Parameter(description = "Offset for paginating through results", required = true)
       @RequestParam Integer offset
   ) {
     return ResponseEntity.status(HttpStatus.OK)
